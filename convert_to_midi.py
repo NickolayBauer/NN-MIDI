0bbbe2b1910d44d72abc57216484a31d5f46aa7e
WAF <kolanlos510@gmail.com>
2019-03-22 12:12:39 +0300

    исправлены косячные функции
    пока пытался объяснить другим, что я сделаю, сделал всё хорошо

--- convert_to_midi.py
+++ convert_to_midi.py
@@ -1,49 +1,6 @@
+from mido import Message, MidiFile, MidiTrack
 import sys
 import key
-
-from mido import Message, MidiFile, MidiTrack
-
-# Используемые функции:
-# note_to_midi() - преобразование массива нот в аудиофайл;
-# midi_to_note() - преобразование аудиофайла в массив нот.
-
-# Используемые переменные:
-# outfile - объект класса MidiFil;
-# track - объект класса MidiTrack;
-# delta - пауза между нотами;
-# default_delta - сохранённая delta;
-# pause_coef - коэффициент паузы;
-# ticks_per_expr - количество ударов в минуту;
-# elem - элемент списка;
-# notes - массив нот;
-# note - нота;
-# pitch - копия основного сигнала;
-# j - индекс;
-# save_file - имя сохраняемого файла;
-# load_file - имя загружамоего файла;
-# list_notes - массив нот;
-# pause_time - время паузы;
-# mid - объект класса MidiFile;
-# msg - элемент mid.
-
-
-#pause = 120 
-#одна нота при 140bpm
-
-# def filter_note(notes):
-# 	clear_list = [notes[0]]
-# 	for i,elem in enumerate(notes):
-# 		try:
-# 			if notes[i-1] == "-":
-# 				if elem == "-":
-# 					clear_list.append(elem)
-# 			else:
-# 				clear_list.append(elem)
-# 		except:
-# 			print(elem)
-# 			clear_list.append(elem)
-# 	return clear_list
-
 
 def note_to_midi(save_file, notes):
 	outfile = MidiFile()
--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-# Отчет по производственной практике 01
-# по теме: Создание API для подготовки выборки, использующейся в нейронной сети.
-# Разработал: Лосев Николай Дмитриевич ТМП-82
-# Используемый язык программирования: Python
-#
-# Задание:
-# Реализовать API, позволяющее преобразовывать midi – файлы в нотную текстовую запись и подготовить данные, которые сможет использовать нейронная сеть. 
-#
-# Используемые функции:
-# convert_to_matrix() - преобразование нотной записи в матрицу;
-# matrix_to_note() - преобразование матрицы в нотную запись;
-# note_to_midi() - преобразование нотной записи в midi - файл;
-# midi_to_note() - преобразование midi - файла в нотную запись.
-
-#Используемые переменные:
-# notes - список нот;
-# notes_str - текстовая запись нот;
-# matrix - матрица созданная из нотной записи.
-
 from convert_to_matrix import convert_to_matrix, matrix_to_note, empty_array
 from convert_to_midi import note_to_midi, midi_to_note
 from read_bin import save_to_file, load_on_file
 from itertools import zip_longest
+import math
 import os
+
+def sigmoid(x):
+	return 1/(1+math.exp(-x))
+
+def gtang(x):
+	return 2/(1+math.exp(-2*x))-1
 
 def create_bin_matrix():
 	path = "samples/"
@@ -32,20 +20,17 @@ def mirror(sample, preset):
 		for i, _ in enumerate(preset):
 			for j, _ in enumerate(preset[0]):
 				weight += sample[i][j] * preset[i][j]
-		return weight
+		return gtang(weight)
 		
 def chunks(lst, count):
     return [list(elem) for elem in zip_longest(*[iter(lst)] * count, fillvalue = [0 for elem in range(len(lst[0]))]) ]
 
-
 def nn(sample, example, chunk):
 	return [mirror(exmpl, smpl) for exmpl, smpl in zip(chunks(load_on_file(example),chunk), chunks(load_on_file(sample),chunk))]
 
+def go():
+	for i in range(5):
+		print(str(i)+".txt: ", nn("example.txt", "text_presets/"+str(i)+".txt", 2))
+	#save_to_file("example",convert_to_matrix(midi_to_note("example.mid"),4))
 
-#for i in range(5):
-#	print(nn("example.txt", "text_presets/"+str(i)+".txt", 2))
-
-
-#save_to_file("example", convert_to_matrix(midi_to_note("example.mid",480),4))
-
-save_to_file("example",convert_to_matrix(midi_to_note("example.mid"),4))
+go()
