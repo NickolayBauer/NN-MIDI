d76b0a8269ebfdb6b7892c054570ca9cc3f9317b
WAF <kolanlos510@gmail.com>
2019-05-28 20:26:39 +0300

    123

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ from convert_to_matrix import matrix_to_note
 from convert_to_midi import note_to_midi
 import numpy as np
 from key import ord_note
-
+import sys
 
 def bedrock(arg):
 	if agr == "work":
-		r_notes = [elem for row in [reset_from_file(elem) for elem in lets_go("work")] for elem in row]
-		notes = (matrix_to_note(r_notes, 4))
-		print([ord_note(elem) for elem in notes])
+		print("режим обучения!")
+		#r_notes = [elem for row in [reset_from_file(elem) for elem in lets_go("work")] for elem in row]
+		#notes = (matrix_to_note(r_notes, 4))
+		#print([ord_note(elem) for elem in notes])
 	
 	elif arg == "train":
-		create_bin_matrix()
-		lets_go("train")
-		note_to_midi("beta.mid", notes)
+		print("режим тренировки!")
+		#create_bin_matrix()
+		#lets_go("train")
+		#note_to_midi("beta.mid", notes)
 
 	else:
-		print("Нет такой команды!")		
+		print("Нет такой команды!")
+
+if __name__ == "__main__":
+    for param in sys.argv:
+        print(param)
