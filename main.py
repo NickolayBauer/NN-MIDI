c3fee24b04c4799ad918a5e0330d4d4133ebd84b
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2019-05-28 21:03:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from create_file_matrix_from_midi import create_bin_matrix
 from reset_from_file import reset_from_file
 from convert_to_matrix import matrix_to_note
 from convert_to_midi import note_to_midi
-import numpy as np
 from key import ord_note
 import sys
 
-def bedrock(arg, name = "result"):
+
+def bedrock(arg, name="result"):
 	if arg == "work":
 		print("режим обучения!")
 		r_notes = [elem for row in [reset_from_file(elem) for elem in lets_go("work")] for elem in row]
 		notes = (matrix_to_note(r_notes, 4))
-		note_to_midi(name+".mid", notes)
+		note_to_midi(name + ".mid", notes)
 
 	elif arg == "train":
 		print("режим тренировки!")
@@ -21,6 +21,7 @@ def bedrock(arg, name = "result"):
 
 	else:
 		print("Необходимо указать корректную команду")
+
 
 if __name__ == "__main__":
 	if len(sys.argv) > 2 and len(sys.argv) < 4:
